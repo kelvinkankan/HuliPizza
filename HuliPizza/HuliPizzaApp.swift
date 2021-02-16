ffdd18635b71154d18c7d1fc62bb65172e294bb7
kelvinkankan <43528189+kelvinkankan@users.noreply.github.com>
2021-02-17 01:32:07 +0800

    Initial Commit

---
+++ HuliPizza/HuliPizzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HuliPizzaApp.swift
+//  HuliPizza
+//
+//  Created by Kevin Kan on 17/2/2021.
+//
+
+import SwiftUI
+
+@main
+struct HuliPizzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
